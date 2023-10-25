8efb82898108445bb651c0b99e323bc17ba59ccc
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-10-25 23:19:08 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,16 @@
+
+
+const addId = 12477
+var addEmail = "dpds@gmail.com";
+let addPassword = "936918";
+
+addCity = "Lucknow";
+let addAddress;   // we can able to declare any variable without any value 
+
+console.log(addId);
+
+// prefer not to use var because of issue in block scope and functional space
+
+
+console.table([addId, addEmail, addPassword, addCity, addAddress]);
+

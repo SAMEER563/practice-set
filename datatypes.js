9ecafab47ed8fb42f491433aabccba4f9205aa81
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-10-26 10:50:38 +0530

    datatype

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+
+
+let name = "Ram";
+let age = 21
+let isLoggedIn = false
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+console.log(age);
+console.log(typeof name);

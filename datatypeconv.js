7256548f02002c0b32f331534504942608115e4b
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2023-10-27 19:59:34 +0530

    data convertion

---
+++ datatypeconv.js
@@ -0,0 +1,19 @@
+
+
+let score = "bdg"
+
+// console.log(typeof score);             **  we commentr these lines because it give more output
+// console.log(typeof (score));
+
+let valueNumber = Number(score);
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+
+// 33 => 33
+// "45hgs" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+let isLoggedInBoolean = Boolean(isLoggedIn);
+console.log(isLoggedInBoolean);

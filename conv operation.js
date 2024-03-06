a8d1a399464f7bbcc89d7177fe920d262ed9aacf
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-06 11:08:17 +0530

    operation

---
+++ conv operation.js
@@ -0,0 +1,58 @@
+
+
+let score = "bdg"
+
+// console.log(typeof score);             **  we commentr these lines because it give more output
+// console.log(typeof (score));
+
+let valueNumber = Number(score);
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+
+// 33 => 33
+// "45hgs" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "sameer"
+let isLoggedInBoolean = Boolean(isLoggedIn);
+// console.log(isLoggedInBoolean);
+
+// 1 => true; 0=> false
+// "" => false
+// "sameer" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***************** Operation ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "avanish"
+let str2 = " ki arushi"
+let str3 = str1  + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log(1 + "2" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1)
+
+
+gameCounter = 100
+++gameCounter;
+console.log(gameCounter)

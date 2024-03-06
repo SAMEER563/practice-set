5eb0e6de874c53129a019f3ff518aceab2c180fb
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-06 11:29:56 +0530

    comparision

---
+++ comparision.js
@@ -0,0 +1,19 @@
+//  console.log(2 > 1)
+//  console.log(2 < 1)
+//  console.log(2 >= 1)
+//  console.log(2 == 1)
+
+
+//  console.log(null > 1)
+//  console.log(null == 0)
+//  console.log(null >= 0)
+//  console.log(null < 1)
+
+console.log(undefined > 0)
+console.log(undefined <= 1)
+console.log(undefined < 1)
+console.log(undefined == 0)
+
+// ===
+
+console.log("2" === 2)

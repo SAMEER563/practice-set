0d6db5dde7dba364347bb75f0f6453c1b2037d5f
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-08 12:43:31 +0530

    nums and math

---
+++ nums_and_math.js
@@ -0,0 +1,37 @@
+ const score = 800
+//  console.log(score)
+
+ const balance = new Number(400)
+//  console.log(balance)
+
+ // convert nuber into string
+
+//  console.log(balance.toString().length);
+//  console.log(balance.toFixed(2));
+
+ // return the precise value
+
+ const otherNumber = 452.4784
+//  console.log(otherNumber.toPrecision(3))
+
+
+ // output seperate with comma
+
+ const hundreds = 100000
+//  console.log(hundreds.toLocaleString())
+
+ //********************************* Math *********************/
+
+//  console.log(Math);
+//  console.log(Math.abs(-4));
+//  console.log(Math.round(4.7));
+//  console.log(Math.sqrt(25));
+//  console.log(Math.min(4, 5, 3, 6, 2))
+
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)

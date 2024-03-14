98d1dadbd891e06cda331879361d9ccbd44fda2c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-14 23:37:48 +0530

    change2

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -30,8 +30,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B", myArr);
+// console.log(myn1);
+// console.log("B", myArr);
 
 const myn2 = myArr.splice(1, 3)
 console.log("C", myArr);

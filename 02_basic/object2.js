162f83ace3da85a723f217587c23ccbd64857543
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-15 19:23:21 +0530

    object2

---
+++ 02_basic/object2.js
@@ -0,0 +1,45 @@
+// const blogUser = new Object()
+
+// console.log(blogUser);
+
+const blogUser = {}
+
+blogUser.id = "123abd"
+blogUser.name ="Sam"
+blogUser.isLoggedIn = false
+
+// console.log(blogUser);
+
+const regularUser ={
+    email: "sam44@google.com",
+    fullname: {
+        userfullname: {
+            firstname: "Sameer",
+            lastname: "Maurya"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {obj1, obj2}
+
+
+// Object.assign is use to merge all value 
+// const obj3 = Object.assign({}, obj1, obj2)
+
+
+// it use to spread the value
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+console.log(blogUser);
+
+console.log(Object.keys(blogUser));
+console.log(Object.values(blogUser));
+console.log(Object.entries(blogUser));
+
+console.log(blogUser.hasOwnProperty('isLogged'));

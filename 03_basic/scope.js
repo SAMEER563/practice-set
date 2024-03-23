1438b7874d80ce939fba073c7fa8e54493ce76f5
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-23 22:08:54 +0530

    scope done

--- 03_basic/scope.js
+++ 03_basic/scope.js
@@ -44,4 +44,14 @@ if (true) {
 }
 
 // console.log(username);
+// ================ interesting =================
+
+function addone(num) {
+    return num + 1
+}
+addTwo(5)
+
+const addTwo = function(num) {
+    return num + 2
+}
 

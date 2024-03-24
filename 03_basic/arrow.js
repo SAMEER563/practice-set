67a1af24e86023cc12178fd74ab05cfe7dc6e710
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-24 22:21:11 +0530

    arrow

--- 03_basic/arrow.js
+++ 03_basic/arrow.js
@@ -4,7 +4,49 @@ const user = {
 
     welcomeMessage: function() {
         console.log(`${this.username}, welcome to website`);
+        //  console.log(this);
     }
 }
 
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// function coffee() {
+//     let username = "sameer"
+//     console.log(this);
+// }
+
+// coffee()
+
+// const coffee = function () {
+//     let username = "sameer"
+//     console.log(this.username);
+// }
+
+// coffee()
+
+// const coffee =  () => {
+//     let username = "sameer"
+//     console.log(this.username);
+// }
+
+// coffee()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) => num1 + num2
+
+// const addTwo = (num1, num2) =>( num1 + num2 )
+
+
+const addTwo = (num1, num2) => ({username: "sameer"})
+
+console.log(addTwo(3, 4));
+
+// const myArray = [2, 4, 8, 5, 4]
+
+
+// myArray.forEach(() => {})

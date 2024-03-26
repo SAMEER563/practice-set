4285324774f45bf81eed8b3e5b338dc9a728a1dc
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-26 22:01:39 +0530

    condtion

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -24,3 +24,30 @@
 // when we use any console outside the scope variable and does not define the value ouside then 
 // it give error  
 // console.log(`User power: ${power}`);
+
+
+
+// =========  short hand notation ========
+
+const balance = 599
+
+// if (balance > 421) console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else {
+//     console.log("greater than 750");
+// }
+
+
+// multiple condition check
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+
+if (userLoggedIn && debitCard && loggedInFromGoogle) {
+    console.log("Allow to buy course");
+}

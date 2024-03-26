78e650a52d8ad2d1c340f5c43246230209f92f6c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-26 18:12:53 +0530

    if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+// if statement
+
+const temperature = 42
+
+if ( temperature > 38 ) {
+    console.log("less than 50");
+} else {
+    console.log("greater than 50");
+}
+
+// operators
+// <, >, =, ==, ===, !=, !==, >=, <=

beb812981406f3847d38620ed9eaeacca544fd0a
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-26 20:03:37 +0530

    scope understand

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,12 +1,26 @@
 // if statement
 
-const temperature = 42
+// const temperature = 42
 
-if ( temperature > 38 ) {
-    console.log("less than 50");
-} else {
-    console.log("greater than 50");
-}
+// if ( temperature > 38 ) {
+//     console.log("less than 50");
+// } else {
+//     console.log("greater than 50");
+// }
 
 // operators
 // <, >, =, ==, ===, !=, !==, >=, <=
+
+
+
+// const score = 452
+
+// if (score >  214) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+
+// when we use any console outside the scope variable and does not define the value ouside then 
+// it give error  
+// console.log(`User power: ${power}`);

38fc6d42da0d5d463e0bc603a8d0c3c26e6d9b0c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-27 11:44:47 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 5
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("value does not match");
+        break;
+}

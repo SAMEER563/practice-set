d6f6f99ece576c2f605031c9b28a4d1507071bd3
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-28 18:56:52 +0530

    change

--- 02_basic/array2.js
+++ 02_basic/array2.js
@@ -1,13 +1,13 @@
 const marvel_heros = ["thor", "ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+marvel_heros.push(dc_heros)
 
-// console.log(marvel_heros);
-// console.log(marvel_heros[1][2]);
+console.log(marvel_heros);
+console.log(marvel_heros[1][2]);
 
-// const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 

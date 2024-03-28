21b51298b237018969c612142a4925a28fe05b4f
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-28 18:50:49 +0530

    loop half

---
+++ 05_iteration/one.js
@@ -0,0 +1,18 @@
+// for
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    if (element == 5) {
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    // for (let j = 0; j < 10; j++) {
+    // //    console.log(`Inner loop value ${j} and inner loop ${i}`);
+    //     console.log(i + '*' + j + ' = ' +  i*j);
+    // }
+}
+

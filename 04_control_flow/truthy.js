cc51345dc8ff115ee379b7a44e40a55da2861020
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-03-31 22:31:20 +0530

    change

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,10 +1,10 @@
-const userEmail = ""
+// const userEmail = ""
 
-if (userEmail) {
-    console.log("Got user email");
-} else {
-    console.log("Don't have user email ");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email ");
+// }
 
 
 // Falsy values 

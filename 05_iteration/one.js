9886d68b27c26b222d6b289309e7f64aa9bef188
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-14 23:29:07 +0530

    change

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -19,10 +19,10 @@
 // let myArray = ["flash", "batman", 'superman']
 // // console.log(myArray.length);
 
-// for (let index = 0; index < myArray.length; index) {
-//     const element = myArray[index];
-//     // console.log(element);
-// }
+for (let index = 0; index < myArray.length; index) {
+    const element = myArray[index];
+    console.log(element);
+}
 
 
 // break and continue

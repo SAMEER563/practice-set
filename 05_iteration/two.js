e7ecb1380637629426545151a42a1582a99f543d
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-19 09:18:47 +0530

    while and do while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,22 @@
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`Value of i is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ["flash", "batman", 'superman']
+
+let arr = 0;
+while (arr < myArray.length) {
+    // console.log(`Value of i is ${myArray[arr]}`);
+    arr = arr + 1;
+    
+}
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score = score + 1;
+} while (score <= 10);

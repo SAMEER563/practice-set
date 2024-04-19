ea83da0d67e3f4170135633c163dc731dd961f50
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-19 09:34:43 +0530

    for of

---
+++ 05_iteration/three.js
@@ -0,0 +1,36 @@
+// for of
+
+const arr = [1, 2, 3, 4, 5];
+for (const num of arr) {
+    console.log(num);
+}
+
+
+const greeting = "Hello World!";
+for (const greet of greeting) {
+    console.log(`Letter: ${greet}`);
+    
+}
+
+// Map
+
+const myMap = new Map();
+myMap.set("IN", "India");
+myMap.set("US", "United States");
+myMap.set("UK", "United Kingdom");
+
+// console.log(myMap);
+
+for (const [key,value] of myMap) {
+    // console.log(key, ":=", value);
+}
+
+const myObject = {
+    game1: 'Cricket',
+    game2: 'Football',
+
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ":=" , value);
+// }

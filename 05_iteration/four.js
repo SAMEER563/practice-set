420c9929cc728616fb5392d17a316a18717d15a8
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-19 10:37:58 +0530

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,31 @@
+// for in loop is used to iterate over the enumerable properties of an object.
+
+
+const myObject = {
+    js: 'JavaScript',
+    py: 'Python',
+    rb: 'Ruby',
+    ts: 'TypeScript'
+};
+
+for (const key in myObject) {
+  
+}
+
+const programmingLanguages = ['JavaScript', 'Python', 'Ruby', 'TypeScript'];
+
+for (const key in programmingLanguages) {
+   
+        
+    // console.log(key);
+}
+
+// const myMap = new Map();
+// myMap.set("IN", "India");
+// myMap.set("US", "United States");
+// myMap.set("UK", "United Kingdom");
+
+// for (const key in myMap) {
+//    console.log(myMap);
+// }
+

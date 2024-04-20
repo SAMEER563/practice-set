ef06e2bbbba428114a2c66652be2242a6ab5f4f8
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-20 22:16:20 +0530

    six

---
+++ 05_iteration/six.js
@@ -0,0 +1,32 @@
+const coding = ["js", "py", "rb", "ts"];
+
+const values = coding.forEach((item) => {
+    // console.log(item);
+    return item;
+});
+
+console.log(values); // undefined
+
+
+const myNums = [1, 2, 3, 4, 5,6, 7, 8, 9, 10];
+
+// const newNums = myNums.filter((num) => num > 5);
+// console.log(newNums);
+
+
+
+// const newNums = myNums.filter((num) => {
+//     return num > 5;
+// });
+// console.log(newNums);
+
+
+
+// const newNums = []
+// myNums.forEach((num) => {
+//     if (num > 5) {
+//         newNums.push(num);
+//     }
+// });
+
+// console.log(newNums);

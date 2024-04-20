b8005ebbc8178012e7c557ff01431700774f7a2c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-04-20 22:16:43 +0530

    six change

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -1,24 +1,24 @@
-const coding = ["js", "py", "rb", "ts"];
+// const coding = ["js", "py", "rb", "ts"];
 
-const values = coding.forEach((item) => {
-    // console.log(item);
-    return item;
-});
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item;
+// });
 
-console.log(values); // undefined
+// console.log(values); // undefined
 
 
 const myNums = [1, 2, 3, 4, 5,6, 7, 8, 9, 10];
 
-// const newNums = myNums.filter((num) => num > 5);
-// console.log(newNums);
+const newNums = myNums.filter((num) => num > 5);
+console.log(newNums);
 
 
 
-// const newNums = myNums.filter((num) => {
-//     return num > 5;
-// });
-// console.log(newNums);
+const newNums = myNums.filter((num) => {
+    return num > 5;
+});
+console.log(newNums);
 
 
 

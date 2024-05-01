3cf02670ae5941a996dfa77dde759326053f41a3
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-01 14:15:19 +0530

    change

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -22,12 +22,12 @@ const myNums = [1, 2, 3, 4, 5,6, 7, 8, 9, 10];
 
 
 
-// const newNums = []
-// myNums.forEach((num) => {
-//     if (num > 5) {
-//         newNums.push(num);
-//     }
-// });
+const newNums = []
+myNums.forEach((num) => {
+    if (num > 5) {
+        newNums.push(num);
+    }
+});
 
 console.log(newNums);
 

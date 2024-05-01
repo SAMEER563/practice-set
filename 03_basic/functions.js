cb9d87200dd73276d6e74ce9f4999fa2e2b4724c
SAMEER KUMAR MAURYA <105592672+SAMEER563@users.noreply.github.com>
2024-05-01 14:17:10 +0530

    change

--- 03_basic/functions.js
+++ 03_basic/functions.js
@@ -15,14 +15,14 @@ function sayName(){
 //     console.log(number1 + number2);
 // }
 
-// function addTwoNumber (number1, number2){
-//    let result = number1 + number2
-//    return result
-//    return number1 + number2
-// }
-// const result = addTwoNumber(4, 8)
+function addTwoNumber (number1, number2){
+   let result = number1 + number2
+   return result
+   return number1 + number2
+}
+const result = addTwoNumber(4, 8)
 
-// console.log("Result:", result);
+console.log("Result:", result);
 
 
 // function loginUserMesssage(username){
